a9615874e627d7fec41b741b0f3be9e1aaf13b12
Narius2030 <buiducnhan15122003@gmail.com>
2024-08-27 14:20:33 +0700

    feat: load raw data from parquet file to bronze

---
+++ airflow/plugins/bronze/load.py
@@ -0,0 +1,37 @@
+import pymongo
+import pandas as pd
+from tqdm import tqdm
+
+client = pymongo.MongoClient("mongodb://admin:nhanbui@localhost:27017/")
+db = client["imcp"]
+
+def load_parquet(file_path, engine='pyarrow'):
+    df = pd.read_parquet(file_path, engine=engine)
+    df['created_time'] = pd.to_datetime('now')
+    df['publisher'] = 'HuggingFace'
+    df['year'] = '2023'
+    df['howpublished'] = 'https://huggingface.co/datasets/laion/220k-GPT4Vision-captions-from-LIVIS'
+    datasets = df.to_dict('records')
+    
+    docs = db['bronze'].find()
+    try:
+        if any(docs) == False:
+            db['bronze'].insert_many(datasets)
+        else:
+            for data in tqdm(datasets):
+                query = {
+                    "url": f"{data['url']}",
+                    "$and": [
+                        {"caption": f"{data['caption']}"},
+                        {"short_caption": f"{data['short_caption']}"}
+                    ]
+                }
+                docs = db['bronze'].find(query, {'url':1,'caption':1, 'short_caption':1})
+                if any(docs) == False:
+                    db['bronze'].insert_one(data)
+    except pymongo.errors.WriteError as ex:
+        raise Exception(str(ex))
+        
+
+if __name__=='__main__':
+    load_parquet('./data/HuggingFace/lvis_caption_url.parquet')
--- submit-spark.py
+++ submit-spark.py
@@ -1,14 +1,27 @@
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.appName("PythonWordCount").getOrCreate()
+# spark = SparkSession.builder.appName("PythonWordCount").getOrCreate()
 
-text = "Hello Spark Hello Python Hello Airflow Hello Docker and Hello Yusuf"
+# text = "Vậy câu hỏi mà mình đặt ra ở đây là, làm sao để kết hợp thông tin của hai nguồn dữ liệu trên. Mình nên xây dựng một mạng nhận cả ảnh và caption vào một lúc, hay nên xử lí từng phần riêng biệt, xử lí ảnh riêng, xử lý caption riêng, rồi concat kết quả lại. Nhưng xử lý ảnh và caption cùng lúc hẳn sẽ cung cấp thêm thông tin cho nhau. Vậy lại phát sinh thêm câu hỏi khác, nên dùng cả ảnh để bổ trợ cho mỗi step, hay nên chia ảnh ra thành các step để fig với độ dài của caption. Liệu như thế có thể lấy hết thông tin của ảnh. Rốt cuộc làm như thế nào mới là tốt. Rất may, mình đã tìm được một bài báo chỉ rõ vấn đề này."
+# words = spark.sparkContext.parallelize(text.split(" "))
+# wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
 
-words = spark.sparkContext.parallelize(text.split(" "))
+# for wc in wordCounts.collect():
+#     print(wc[0], wc[1])
 
-wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
+# spark.stop()
 
-for wc in wordCounts.collect():
-    print(wc[0], wc[1])
+from pyspark.sql import SparkSession
 
-spark.stop()
+# create a local SparkSession
+spark = SparkSession.builder \
+                .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:3.0.1") \
+                .appName("readExample") \
+                .getOrCreate()
+
+# define a streaming query
+dataStreamWriter = spark.read.format("com.mongodb.spark.sql.DefaultSource") \
+                        .option('spark.mongodb.input.uri', 'mongodb://admin:nhanbui@localhost:27017/test.coffeeshop?authSource=admin') \
+                        .load()
+
+print(dataStreamWriter.printSchema())
