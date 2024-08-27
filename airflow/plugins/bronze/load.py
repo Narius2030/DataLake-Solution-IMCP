3dfd3757a946f2ab0e53755fc2ae5e09d1af21cb
Narius2030 <buiducnhan15122003@gmail.com>
2024-08-27 14:28:12 +0700

    feat: load raw data from parquet file to bronze

--- airflow/plugins/bronze/load.py
+++ airflow/plugins/bronze/load.py
@@ -2,8 +2,6 @@ import pymongo
 import pandas as pd
 from tqdm import tqdm
 
-client = pymongo.MongoClient("mongodb://admin:nhanbui@localhost:27017/")
-db = client["imcp"]
 
 def load_parquet(file_path, engine='pyarrow'):
     df = pd.read_parquet(file_path, engine=engine)
@@ -13,24 +11,26 @@ def load_parquet(file_path, engine='pyarrow'):
     df['howpublished'] = 'https://huggingface.co/datasets/laion/220k-GPT4Vision-captions-from-LIVIS'
     datasets = df.to_dict('records')
     
-    docs = db['bronze'].find()
-    try:
-        if any(docs) == False:
-            db['bronze'].insert_many(datasets)
-        else:
-            for data in tqdm(datasets):
-                query = {
-                    "url": f"{data['url']}",
-                    "$and": [
-                        {"caption": f"{data['caption']}"},
-                        {"short_caption": f"{data['short_caption']}"}
-                    ]
-                }
-                docs = db['bronze'].find(query, {'url':1,'caption':1, 'short_caption':1})
-                if any(docs) == False:
-                    db['bronze'].insert_one(data)
-    except pymongo.errors.WriteError as ex:
-        raise Exception(str(ex))
+    with pymongo.MongoClient("mongodb://admin:nhanbui@localhost:27017/") as client:
+        db = client["imcp"]
+        docs = db['bronze'].find()
+        try:
+            if any(docs) == False:
+                db['bronze'].insert_many(datasets)
+            else:
+                for data in tqdm(datasets):
+                    query = {
+                        "url": f"{data['url']}",
+                        "$and": [
+                            {"caption": f"{data['caption']}"},
+                            {"short_caption": f"{data['short_caption']}"}
+                        ]
+                    }
+                    docs = db['bronze'].find(query, {'url':1,'caption':1, 'short_caption':1})
+                    if any(docs) == False:
+                        db['bronze'].insert_one(data)
+        except pymongo.errors.WriteError as ex:
+            raise Exception(str(ex))
         
 
 if __name__=='__main__':
