88f348df0fb51ba44c98131541844116225110a6
Narius2030 <buiducnhan15122003@gmail.com>
2024-08-27 18:11:28 +0700

    feat: create audit collection for write logs

--- airflow/plugins/bronze/load.py
+++ airflow/plugins/bronze/load.py
@@ -1,7 +1,28 @@
 import pymongo
 import pandas as pd
 from tqdm import tqdm
+import json
 
+
+with open("./airflow/config.json", "r") as file:
+    config = json.load(file)
+    db_config = config["db_params"]
+    src_config = config["source"]
+
+def audit_log(start_time, end_time, status, error_message="", affected_rows=0, action=""):
+    with pymongo.MongoClient(f"mongodb://{db_config['user']}:{db_config['password']}@{db_config['endpoint_local']}:27017/") as client:
+        db = client['imcp']
+        log = {
+            "layer": "bronze",
+            "table_name": "bronze",
+            "start_time": start_time,
+            "end_time": end_time,
+            "status": status,
+            "error_message": error_message,
+            "affected_rows": affected_rows,
+            "action": action
+        }
+        db['audit'].insert_one(log)
 
 def load_parquet(file_path, engine='pyarrow'):
     df = pd.read_parquet(file_path, engine=engine)
@@ -11,13 +32,19 @@ def load_parquet(file_path, engine='pyarrow'):
     df['howpublished'] = 'https://huggingface.co/datasets/laion/220k-GPT4Vision-captions-from-LIVIS'
     datasets = df.to_dict('records')
     
-    with pymongo.MongoClient("mongodb://admin:nhanbui@localhost:27017/") as client:
+    with pymongo.MongoClient(f"mongodb://{db_config['user']}:{db_config['password']}@{db_config['endpoint_local']}:27017/") as client:
         db = client["imcp"]
-        docs = db['bronze'].find()
+        # start to load
+        start_time = pd.to_datetime('now')
+        affected_rows = 0
         try:
+            docs = db['bronze'].find()
             if any(docs) == False:
-                db['bronze'].insert_many(datasets)
+                # If there is empty collection -> insert all
+                resp = db['bronze'].insert_many(datasets)
+                affected_rows = len(resp.inserted_ids)
             else:
+                # If there are several documents -> check duplication -> insert one-by-one
                 for data in tqdm(datasets):
                     query = {
                         "url": f"{data['url']}",
@@ -26,12 +53,18 @@ def load_parquet(file_path, engine='pyarrow'):
                             {"short_caption": f"{data['short_caption']}"}
                         ]
                     }
-                    docs = db['bronze'].find(query, {'url':1,'caption':1, 'short_caption':1})
+                    docs = db['bronze'].find(query, {"url":1,"caption":1,"short_caption":1})
                     if any(docs) == False:
                         db['bronze'].insert_one(data)
-        except pymongo.errors.WriteError as ex:
+            # Write logs
+            audit_log(start_time, pd.to_datetime('now'), status="SUCCESS", action="insert", affected_rows=affected_rows)
+
+        except Exception as ex:
+            # Write logs
+            audit_log(start_time, pd.to_datetime('now'), status="ERROR", error_message=str(ex), action="insert", affected_rows=affected_rows)
+            # Raise error
             raise Exception(str(ex))
         
 
 if __name__=='__main__':
-    load_parquet('./data/HuggingFace/lvis_caption_url.parquet')
+    load_parquet('./airflow/data/HuggingFace/lvis_caption_url.parquet')
