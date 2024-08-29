b3e0bc129cd659832c3a4af4aba86a4f28c2a61d
Narius2030 <buiducnhan15122003@gmail.com>
2024-08-29 11:51:35 +0700

    feat: normalize captions in silver layer

---
+++ airflow/functions/text/clean.py
@@ -0,0 +1,36 @@
+from pyspark.sql import Row
+import re
+
+# lower case captions
+def lower_case(row):
+    lowered_caption = row['caption'].lower()
+    lowered_shrtcaption = row['short_caption'].lower()
+    return Row(_id=row['_id'], 
+                caption=lowered_caption, 
+                created_time=row['created_time'], 
+                howpublished=row['howpublished'],
+                publisher=row['publisher'], 
+                short_caption=lowered_shrtcaption, 
+                url=row['url'], year=row['year'])
+
+def remove_punctuations(row):
+    caption = re.sub(r'[^\w\d\s]', '', row['caption'])
+    shrt_caption = re.sub(r'[^\w\d\s]', '', row['short_caption'])
+    return Row(_id=row['_id'],
+                caption=caption, 
+                created_time=row['created_time'], 
+                howpublished=row['howpublished'],
+                publisher=row['publisher'], 
+                short_caption=shrt_caption, 
+                url=row['url'], year=row['year'])
+    
+def tokenize(row):
+    caption = row['caption'].split()
+    shrt_caption = row['short_caption'].split()
+    return Row(_id=row['_id'],
+                caption=caption, 
+                created_time=row['created_time'], 
+                howpublished=row['howpublished'],
+                publisher=row['publisher'], 
+                short_caption=shrt_caption, 
+                url=row['url'], year=row['year'])
--- airflow/plugins/silver/normalize.py
+++ airflow/plugins/silver/normalize.py
@@ -1,14 +1,28 @@
 from pyspark.sql import SparkSession
+from functions.text import clean
 
-# create a local SparkSession
-spark = SparkSession.builder \
-                .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:3.0.1") \
-                .appName("readExample") \
-                .getOrCreate()
 
-# define a streaming query
-bronze_df = spark.read.format("com.mongodb.spark.sql.DefaultSource") \
-                    .option('spark.mongodb.input.uri', 'mongodb://admin:nhanbui@localhost:27017/imcp.bronze?authSource=admin') \
-                    .load()
+    
+def normalize_caption():
+    # create a local SparkSession
+    spark = SparkSession.builder \
+                    .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:3.0.1") \
+                    .appName("readExample") \
+                    .getOrCreate()
 
-print(bronze_df.printSchema())
+    # define a streaming query
+    bronze_df = spark.read.format("com.mongodb.spark.sql.DefaultSource") \
+                        .option('spark.mongodb.input.uri', 'mongodb://admin:nhanbui@localhost:27017/imcp.bronze.parquet?authSource=admin') \
+                        .load()
+
+    bronze_rdd = bronze_df.rdd
+    
+    bronze_rdd = bronze_rdd.map(clean.lower_case)
+    bronze_rdd = bronze_rdd.map(clean.remove_punctuations)
+    silver_rdd = bronze_rdd.map(clean.tokenize)
+    print(silver_rdd.take(2))
+    
+    spark.stop()
+    
+if __name__=='__main__':
+    normalize_caption()
