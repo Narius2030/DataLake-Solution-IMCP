def102c0f406f76d25fa8ec984d2f84e697b083a
Narius2030 <buiducnhan15122003@gmail.com>
2024-09-25 15:28:28 +0700

    feat: encode_image and load to elasticsearch index

--- airflow/plugins/gold/encode_image.py
+++ airflow/plugins/gold/encode_image.py
@@ -3,28 +3,59 @@ sys.path.append('./airflow')
 
 import os
 from functions.images.detr.util.features import get_detr_model, extract_detr_features
-from functions.images.yolo.util.features import extract_yolo_features
-from functions.images.detr.util.transform import compose_shape
+from functions.images.yolo.util.features import extract_yolo_features, get_Net_yolov4
+from functions.images.detr.util.transform import reshape
+
+import pymongo
+import json
+import requests
+import numpy as np
+from tqdm import tqdm
+import base64
+from elasticsearch import Elasticsearch
 
 
-# Tải mô hình DETR
-model, postprocessor = get_detr_model(pretrained=True)
+with open("./airflow/config/env.json", "r") as file:
+    config = json.load(file)
+    mongo_url = config['mongodb']['MONGO_ATLAS_PYTHON_GCP']
+    hugg_index_key = config['elastic']['HUGGINGFACE_INDEX_KEY']
+    hugg_host = config['elastic']['ELASTIC_HOST']
+    
 
-# Khởi tạo một dictionary để lưu trữ các đặc trưng của ảnh
-image_features = {}
+def encode():
+    # Tải mô hình DETR
+    detr_model, postprocessor = get_detr_model(pretrained=True)
+    yolo_model = get_Net_yolov4("./airflow/functions/images/yolo/model/yolov4.weights", "./airflow/functions/images/yolo//model/yolov4.cfg")
+    # Khởi tạo một dictionary để lưu trữ các đặc trưng của ảnh
+    caches = []
 
-# Lặp qua từng ảnh trong thư mục
-for img_name in os.listdir(INPUT_DIR):
-    img_path = os.path.join(INPUT_DIR, img_name)
-    
-    # Trích xuất đặc trưng từ YOLO
-    yolo_features = extract_yolo_features(img_path)
-    
-    # Tải và chuyển đổi hình ảnh đầu vào cho DETR
-    image_tensor = compose_shape(img_path)
-    
-    # Trích xuất đặc trưng từ DETR
-    detr_features = extract_detr_features(image_tensor, model)
-    
-    # Lưu trữ các đặc trưng trong dictionary với ID ảnh là key
-    image_id = img_name.split('.')[0]
+    # Lặp qua từng ảnh trong thư mục
+    with pymongo.MongoClient(mongo_url) as client:
+        db = client['imcp']
+        pipeline = [{
+                '$sort': {'url': 1}
+            }, {
+                '$project': {'created_time': 0, 'publisher': 0, '_id': 0 }
+            }, {
+                '$limit': 8000
+            }
+        ]
+        documents = db['refined'].aggregate(pipeline)
+        for doc in tqdm(documents):
+            try:
+                response = requests.get(doc['url'], timeout=0.5)
+                # Trích xuất đặc trưng từ YOLO
+                yolo_features = extract_yolo_features(yolo_model, response.content)
+                doc['yolov4_encode'] = base64.b64encode(yolo_features)
+                
+                # Trích xuất đặc trưng từ DETR
+                image_tensor = reshape(response.content)
+                detr_features = extract_detr_features(image_tensor, detr_model)
+                doc['detr_encode'] = base64.b64encode(detr_features)
+                caches.append(doc)
+                
+                with Elasticsearch(hosts=hugg_host, api_key=hugg_index_key) as es:
+                    es.update(index="huggingface-index", doc=doc, id=doc['url'][-16:-4], doc_as_upsert=True, upsert=doc)    
+                    
+            except Exception as exc:
+                continue
